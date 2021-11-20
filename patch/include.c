bcb03670f0e508f0e24ef2b2153bd1cb43d366c3
Szekely Attila <attila.szekely@higg.com>
2021-11-21 00:06:15 +0200

    need to add fullscreen

---
+++ patch/include.c
@@ -0,0 +1,21 @@
+/* Bar functionality */
+#include "bar_indicators.c"
+#include "bar_tagicons.c"
+
+#include "bar_ltsymbol.c"
+#include "bar_status.c"
+#include "bar_tags.c"
+#include "bar_wintitle.c"
+
+/* Other patches */
+#include "attachx.c"
+#include "movestack.c"
+#include "pertag.c"
+#include "shiftviewclients.c"
+#include "vanitygaps.c"
+/* Layouts */
+#include "layout_facts.c"
+#include "layout_flextile-deluxe.c"
+#include "layout_monocle.c"
+#include "layout_tile.c"
+
